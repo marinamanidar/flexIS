ca5cbb2380a50863394b6adb043d44b2b80b2222
marinamanidar <marinamanidar13@gmail.com>
2023-04-08 01:41:23 +0800

    dev - fix email unique

--- backend/models/employee.js
+++ backend/models/employee.js
@@ -1,17 +1,14 @@
 const mongoose = require ('mongoose');
-const uniqueValidator = require('mongoose-unique-validator')
 
 const employeeSchema = mongoose.Schema({
   password : { type:String , required : true},
   name: {type: String, required: true},
   position : { type:String , required : true},
-  email: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
   FWAstatus : { type:String , required : true},
   supervisorID : { type:String , required : true},
   departmentID: {type: String, required: true},
   status : { type:String , required : true}
 });
-
-employeeSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Employee', employeeSchema );
